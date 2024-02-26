9684d686b85aacb670263c8286cf8e8cd618781f
Mikhail Menteshynov <menteshynov.mm@gmail.com>
2024-02-26 17:44:36 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Scene from './components/Scene';
+
+function App() {
+  return (
+    <div className="relative h-screen bg-orange-50">
+      <Scene />
+    </div>
+  );
+}
+
+export default App;
