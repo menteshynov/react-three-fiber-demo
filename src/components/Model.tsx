31b5caf684d33baef7cebaf5801ee71a556814a0
Mikhail Menteshynov <menteshynov.mm@gmail.com>
2024-02-26 17:45:54 +0200

    Add model

---
+++ src/components/Model.tsx
@@ -0,0 +1,76 @@
+import { useGLTF } from '@react-three/drei';
+import * as THREE from 'three';
+import { GLTF } from 'three-stdlib';
+
+type GLTFResult = GLTF & {
+  nodes: {
+    Cube001: THREE.Mesh;
+    Cube002: THREE.Mesh;
+    Cylinder: THREE.Mesh;
+    Cube: THREE.Mesh;
+    Cube003: THREE.Mesh;
+    Cylinder001: THREE.Mesh;
+  };
+  materials: {
+    ['Material.001']: THREE.MeshStandardMaterial;
+    ['Material.002']: THREE.MeshStandardMaterial;
+    ['Material.003']: THREE.MeshStandardMaterial;
+    ['Material.004']: THREE.MeshStandardMaterial;
+  };
+};
+
+function Model(props: JSX.IntrinsicElements['group']) {
+  const { nodes, materials } = useGLTF('/Panel.glb') as GLTFResult;
+
+  return (
+    <group {...props} dispose={null}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Cube001.geometry}
+        material={materials['Material.001']}
+        position={[0, 0.196, 0]}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Cube002.geometry}
+        material={materials['Material.001']}
+        position={[0, -0.194, 0]}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Cylinder.geometry}
+        material={materials['Material.002']}
+        position={[-0.5, 0, 0]}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Cube.geometry}
+        material={materials['Material.003']}
+        position={[0, 0.196, 0]}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Cube003.geometry}
+        material={materials['Material.003']}
+        position={[0, -0.194, 0]}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Cylinder001.geometry}
+        material={materials['Material.004']}
+        position={[-0.5, 0, 0]}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+    </group>
+  );
+}
+
+export default Model;
+// useGLTF.preload("/Panel.glb");
